3660b3885b55ce07ffde39597ef8154ac16f4f3e
cjournois <cjournois@assist-evolution.com>
2020-12-07 17:25:59 +0100

    refactor(write): remove old code

--- src/helpers/write.js
+++ src/helpers/write.js
@@ -1,19 +1,6 @@
-import excel4node from 'excel4node'
 import fs from 'fs'
 
 import { Category } from 'utils/enums'
-
-function writeFileWithExcel4Node(data) {
-	const xls = new excel4node.Workbook()
-	const workSheet = xls.addWorksheet('Result')
-
-	Object.keys(data).forEach((categoryKey, index) => {
-		workSheet.cell(1, index + 1).string(String(Object.keys(Category)[categoryKey]))
-		workSheet.cell(2, index + 1).number(data[categoryKey])
-	})
-
-	xls.write('./result/tlor.xlsx')
-}
 
 function writeFileWithFs(data) {
 	const result = Object.keys(data).reduce((res, categoryKey, index) => {
@@ -25,9 +12,7 @@ function writeFileWithFs(data) {
 	})
 }
 
-
 function write(data) {
-	//writeFileWithExcel4Node(data)
 	writeFileWithFs(data)
 }
 
