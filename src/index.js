656f8c95968e1fc9e2c9a207c65f2e3b327f969d
cjournois <cjournois@assist-evolution.com>
2020-12-07 14:17:56 +0100

    feat: initial project

---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+function read() {
+	return []
+}
+
+
+function format(data) {
+	return []
+}
+
+
+function map(data) {
+	return []
+}
+
+
+function reduce(data) {
+	return []
+}
+
+
+function write(data) {
+	console.log('void')
+}
+
+function init() {
+	let data = read()
+	data = format(data)
+	data = map(data)
+	data = reduce(data)
+	write(data)
+}
