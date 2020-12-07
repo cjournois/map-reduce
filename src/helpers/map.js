2b33074b52ec00df40d8909e03236ca9029254e0
cjournois <cjournois@assist-evolution.com>
2020-12-07 15:46:27 +0100

    fix(map): getWordResult

--- src/helpers/map.js
+++ src/helpers/map.js
@@ -1,17 +1,23 @@
 import { Category } from 'utils/enums'
 
+function getWordResult(map, word, category) {
+	if (typeof map[word] === 'object') {
+		return map[word].concat(category)
+	}
+	return [].concat(category)
+}
+
 function map(data) {
 	const map = {}
 	data.forEach((word) => {
 		if (word.length > 10) {
-			map[word] = Category.Large
+			map[word] = getWordResult(map, word, Category.Large)
 		} else if (word.length < 5) {
-			map[word] = Category.Short
+			map[word] = getWordResult(map, word, Category.Short)
 		} else {
-			map[word] = Category.Medium
+			map[word] = getWordResult(map, word, Category.Medium)
 		}
 	})
-
 	return map
 }
 
