d3a945cca862815789d57d1d31edb49c24450d5a
cjournois <cjournois@assist-evolution.com>
2020-12-08 09:47:23 +0100

    feat(log): add log in write

--- src/helpers/write.js
+++ src/helpers/write.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 import { Category } from 'utils/enums'
 
 function writeFileWithFs(data) {
+	console.log('result:', data)
 	const result = Object.keys(data).reduce((res, categoryKey, index) => {
 		return `${res + String(Object.keys(Category)[categoryKey])}\t${data[categoryKey]}\n`
 	}, '')
